93903642233ae4799daa40a3eee905086eb197f1
jmpeoples <jmpeoples.88@gmail.com>
2023-05-24 14:56:04 -0500

    Add correct target blank rel link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -131,6 +131,7 @@ export default function Example() {
               </a>
               <a
                 target='_blank'
+                rel="noreferrer"
                 href="https://twitter.com/sailingfox1"
                 className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
               >
