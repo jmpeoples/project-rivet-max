c58c8b933809e5309979f1d2d4ee451adc4dde64
jmpeoples <jmpeoples.88@gmail.com>
2023-07-14 14:35:22 -0500

    Update steam page link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -124,10 +124,10 @@ export default function Example() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#Story"
+                href="https://store.steampowered.com/app/2491810/Shadow_of_Mammon/"
                 className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
               >
-                Learn more
+                Learn More
               </a>
               <a
                 target='_blank'
